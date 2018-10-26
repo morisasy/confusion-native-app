b04fa634d68c4dc2956e2cc6da8310b40ed6e554
Medi Risasi <morisasy@gmail.com>
2018-10-26 22:23:44 +0300

    navigation part 1

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,30 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
 
-function Menu(props) {
+import { DISHES } from '../shared/dishes';
 
-    const renderMenuItem = ({item, index}) => {
 
-        return (
-            <ListItem
-                key={index}
-                title={item.name}
-                subtitle={item.description}
-                hideChevron={true}
-                onPress={() => props.onPress(item.id)}
-                leftAvatar={{ source: require('./images/vadonut.png')}}
-            />
-        );
+
+
+class Menu extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
+
+    static navigationOptions = {
+        title: 'Menu'
     };
 
-    return (
-            <FlatList 
-                data={props.dishes}
-                renderItem={renderMenuItem}
-                keyExtractor={item => item.id.toString()}
+    render() {
+
+        const renderMenuItem = ({item, index}) => {
+
+            return (
+                <ListItem
+                    key={index}
+                    title={item.name}
+                    subtitle={item.description}
+                    hideChevron={true}
+                    onPress={() => navigate('Dishdetail', { dishId: item.id })}
+                    leftAvatar={{ source: require('./images/vadonut.png')}}
                 />
-    );
+            );
+        };
+
+        const { navigate } = this.props.navigation;
+
+        return (
+                <FlatList 
+                    data={this.state.dishes}
+                    renderItem={renderMenuItem}
+                    keyExtractor={item => item.id.toString()}
+                    />
+        );
+    }
 }
 
 
