8902d9899e435c403aac14ae6024aee2299a584a
Medi Risasi <morisasy@gmail.com>
2018-10-27 12:38:07 +0300

    Commit navigation part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -5,24 +5,44 @@ import Dishdetail from './DishDetailComponent';
 import { DISHES } from '../shared/dishes';
 import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
+import Home from './HomeComponent';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import { Icon } from 'react-native-elements';
 
-const MenuNavigator = createStackNavigator({
-        Menu: { screen: Menu },
-        Dishdetail: { screen: Dishdetail }
-    },
-    {
-        initialRouteName: 'Menu',
+const HomeNavigator = createStackNavigator({
+    Home: { screen: Home }
+  }, {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+          backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+          color: "#fff"            
+      },
+      headerTintColor: "#fff"  
+    })
+});
+
+const MainNavigator = createDrawerNavigator({
+    Home: 
+      { screen: HomeNavigator,
         navigationOptions: {
-            headerStyle: {
-                backgroundColor: "#512DA8"
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-                color: "#fff"            
-            }
+          title: 'Home',
+          drawerLabel: 'Home'
         }
-    }
-);
+      },
+    Menu: 
+      { screen: MenuNavigator,
+        navigationOptions: {
+          title: 'Menu',
+          drawerLabel: 'Menu'
+        }, 
+      }
+}, {
+  drawerBackgroundColor: '#D1C4E9'
+});
+
+
 
 
 class Main extends Component {
@@ -41,7 +61,7 @@ class Main extends Component {
  
     return (
       <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-            <MenuNavigator />
+            <MainNavigator />
         </View>
     );
   }
