c073ba138cb8769569f6a36631076b4f57544125
Medi Risasi <morisasy@gmail.com>
2018-11-22 16:25:47 +0200

    Delete Favorites

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -156,6 +156,11 @@ export const addFavorite = (dishId) => ({
     payload: dishId
 });
 
+export const deleteFavorite = (dishId) => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: dishId
+});  
+
 export const addComment = (comment) => ({
   
     type: ActionTypes.ADD_COMMENT,
