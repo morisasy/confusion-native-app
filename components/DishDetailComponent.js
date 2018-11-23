19d978423667d30046982218d41b24aed5060715
Medi Risasi <morisasy@gmail.com>
2018-11-23 17:07:41 +0200

    Gestures 2

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -68,6 +68,9 @@ function RenderComments(props) {
 function RenderDish(props) {
 
     const dish = props.dish;
+
+    handleViewRef = ref => this.view = ref;
+
     
     const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
         if ( dx < -200 )
@@ -80,6 +83,8 @@ function RenderDish(props) {
         onStartShouldSetPanResponder: (e, gestureState) => {
             return true;
         },
+        onPanResponderGrant: () => {this.view.rubberBand(1000).then(endState => console.log(endState.finished ? 'finished' : 'cancelled'));},
+
         onPanResponderEnd: (e, gestureState) => {
             console.log("pan responder end", gestureState);
             if (recognizeDrag(gestureState))
@@ -99,10 +104,12 @@ function RenderDish(props) {
 
     if (dish != null) {
         return(
-            <Animatable.View
-                animation="fadeInDown"
-                duration={2000}
+            
+            <Animatable.View 
+                animation="fadeInDown" 
+                duration={2000} 
                 delay={1000}
+                ref={this.handleViewRef}
                 {...panResponder.panHandlers}>
                     <Card
                         featuredTitle={dish.name}
@@ -150,6 +157,7 @@ class DishDetail extends Component {
             comment: "",
         };
         
+        
     }
 
     static navigationOptions = {
@@ -174,6 +182,7 @@ class DishDetail extends Component {
         this.props.postComment(dishId, rating, author, comment);
         this.toggleModal();
     }
+    
         
     resetForm() {
         this.setState({
